fbe37afecb6731913b2485c3e6aa9717ba4a6086
unknown <sonardevplus@gmail.com>
2023-11-18 10:29:23 -0300

    adicao plugin pom

--- src/main/java/br/com/devops/miranda/oladevopsme/IndexController.java
+++ src/main/java/br/com/devops/miranda/oladevopsme/IndexController.java
@@ -1,11 +1,12 @@
 package br.com.devops.miranda.oladevopsme;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/v1" )
 @RestController
+@RequestMapping("/v1")
 public class IndexController {
 
     @GetMapping
